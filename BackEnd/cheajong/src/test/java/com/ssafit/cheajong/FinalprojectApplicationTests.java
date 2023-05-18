42f68542eecc7ef7288ad0f6f80a9da0482e4767
이종민 <john0513@naver.com>
2023-05-18 11:12:58 +0900

    base

---
+++ BackEnd/cheajong/src/test/java/com/ssafit/cheajong/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafit.cheajong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
