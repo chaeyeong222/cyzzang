8522a61d8e0469d502fa212612ce7a9011673193
john0513 <john0513@naver.com>
2023-05-20 00:37:58 +0900

    ss

--- front-app/src/App.vue
+++ front-app/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <header-nav />
+    <router-view />
+    <svg class="cursor" width="20" height="20" viewBox="0 0 20 20">
+      <circle class="cursor__inner" cx="10" cy="10" r="5" />
+    </svg>
   </div>
 </template>
 
+<script>
+import HeaderNav from "./components/common/HeaderNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderNav,
+  },
+};
+
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
