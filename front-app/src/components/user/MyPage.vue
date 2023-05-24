3068d48d3dec1e4d0c7d66b0f294293e25ece930
chaeyeong222 <iky1505@naver.com>
2023-05-24 11:13:49 +0900

    마이페이지 로그인 유저 연결하기

--- front-app/src/components/user/MyPage.vue
+++ front-app/src/components/user/MyPage.vue
@@ -5,23 +5,23 @@
         <form @submit="updateUserInfo">
             <div class="form-group">
             <label for="userId">아이디</label>
-            <input type="text" id="userId" v-model="user.userId" disabled>
+            <input type="text" id="userId" v-model="loginUser.userId" disabled>
           </div> 
           <div class="form-group">
             <label for="nickName">닉네임</label>
-            <input type="text" id="nickName" v-model="user.nickName">
+            <input type="text" id="nickName" v-model="loginUser.nickName">
           </div>  
           <div class="form-group">
             <label for="emailAdress">이메일</label>
-            <input type="email" id="emailAdress"  v-model="user.emailAdress" disabled>
+            <input type="email" id="emailAdress"  v-model="loginUser.emailAdress" disabled>
           </div>
           <div class="form-group">
             <label for="height">키</label>
-            <input type="number" id="height" v-model="user.height" required>
+            <input type="number" id="height" v-model="loginUser.height" required>
           </div>
           <div class="form-group">
             <label for="weight">몸무게</label>
-            <input type="number" id="weight" v-model="user.weight" required>
+            <input type="number" id="weight" v-model="loginUser.weight" required>
           </div> 
           <button type="submit">저장</button>
         </form>
@@ -37,17 +37,19 @@
   </template>
   
   <script>
+  import { mapState } from 'vuex';
+
   export default {
     data() {
       return {
-        user: {
-          userId : "aa",
-          nickName: "John",
-          emailAdress: "johndoe@example.com", 
-          height: 0,
-          weight:0,
+        // user: {
+        //   userId : "aa",
+        //   nickName: "John",
+        //   emailAdress: "johndoe@example.com", 
+        //   height: 0,
+        //   weight:0,
           
-        },
+        // },
         wishlist: [
           { id: 1, name: "상품 1" },
           { id: 2, name: "상품 2" },
@@ -55,15 +57,17 @@
         ]
       };
     },
+    computed:{
+      ...mapState(["loginUser"]),
+    },
     methods: {
       updateUserInfo() { 
-
         let user = {
-        userId: this.user.userId, 
-        nickName: this.user.nickName,
-        emailAdress: this.user.emailAdress,
-        height: this.user.height,
-        weight: this.user.weight,
+        userId: this.loginUser.userId, 
+        nickName: this.loginUser.nickName,
+        emailAdress: this.loginUser.emailAdress,
+        height: this.loginUser.height,
+        weight: this.loginUser.weight,
         }  
         alert("개인정보가 업데이트되었습니다.");
         this.$store.dispatch("updateUser", user);
